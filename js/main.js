276f5c844de7eaf52901c5c7c91ab01801435592
wanjar <wanjangunjiri@gmail.com>
2021-10-05 20:56:29 +0300

    Changes to filter

--- js/main.js
+++ js/main.js
@@ -148,7 +148,8 @@ jQuery(document).ready(function( $ ) {
 
   $('#portfolio-flters li').on( 'click', function() {
     $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+   $(this).addClass('filter-active');
+   
 
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
   });
